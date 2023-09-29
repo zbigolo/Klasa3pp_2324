e0548d9134e1acaf383ccc112e78518087f05a0a
zbigolo <50556058+zbigolo@users.noreply.github.com>
2023-09-29 09:21:58 +0200

    Update Sample001JLabel.java

--- src/Z4ElementsGUI/Sample001JLabel.java
+++ src/Z4ElementsGUI/Sample001JLabel.java
@@ -4,6 +4,7 @@ public class Sample001JLabel {
 
          public JPanel myContentPane () {
              JPanel myPanel = new JPanel(null);
+                  // tutaj tworzymy obiekty
              return myPanel;
         }
 
