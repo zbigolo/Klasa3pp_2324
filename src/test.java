fb41120d08e81da191a627d3d515d9c8da11b596
zbigolo <50556058+zbigolo@users.noreply.github.com>
2023-11-24 13:42:16 +0100

    Update test.java

--- src/test.java
+++ src/test.java
@@ -2,7 +2,14 @@ public class test {
 
     public static void main(String[] args) {
 
-        //
+        // dodano  
+
+         System.out.println("Witaj świecie");
+         System.out.println("Witaj świecie");
+         System.out.println("Witaj świecie");
+         System.out.println("Witaj świecie");
+
+         System.out.println("Zbigniew Gołębiewski");
 
     }
 }
