f52e167fb8198b605d3bea0ef621608ad14c8e8c
Zbigniew Gołębiewski <zbigolo@gazeta.pl>
2024-01-31 12:21:23 +0100

    update Sample003BListner.java
    Signed-off-by: zbigolo <zbigolo@gazeta.pl>

--- src/Z4ElementsGUI/Sample003BListner.java
+++ src/Z4ElementsGUI/Sample003BListner.java
@@ -2,10 +2,12 @@ package Z4ElementsGUI;
 
 // zastosowanie listnera dla wszystkich
 
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 
 
