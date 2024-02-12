cb61d8c92b56cce813325b9d7ad0489f359647b5
Zbyszek <zbigolo@gazeta.pl>
2024-02-12 19:06:14 +0100

    modify file  java MenagerBorderLayout.java

--- src/Z5Managers/MenagerBorderLayout.java
+++ src/Z5Managers/MenagerBorderLayout.java
@@ -1,6 +1,8 @@
 package Z5Managers;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,17 +10,19 @@ import java.awt.event.ActionListener;
 public class MenagerBorderLayout {
     public JPanel createContentPane(){
 
-        BorderLayout layoutborder = new BorderLayout(40,100);
+        BorderLayout layoutborder = new BorderLayout();
         JPanel panelTestowy = new JPanel(layoutborder);
 
-        JButton button1 = new JButton("Button 1 - PAGE START");
-        JButton button2 = new JButton("Button 2 - PAGE END");
-        JButton button3 = new JButton("Button 3 - LINE START");
-        JButton button4 = new JButton("Button 4 - LINE END");
+        JButton button1 = new JButton("Button 1 - PAGE START (NORTH)");
+        JButton button2 = new JButton("Button 2 - PAGE END (SOUTH)");
+        JButton button3 = new JButton("Button 3 - LINE START (WEST)");
+        JButton button4 = new JButton("Button 4 - LINE END (EAST)");
         JButton button5 = new JButton("Button 5 - CENTER");
 
         JButton button3A = new JButton("Button 3A - RED");
+        button3A.setBorder(new EmptyBorder(3, 0, 3, 0));
         JButton button3B = new JButton("Button 3B - GREEN");
+        button3B.setBorder(new LineBorder(Color.white, 3,true));
 
         JPanel yellow = new JPanel(new FlowLayout());
         yellow.setBackground(Color.yellow);
@@ -42,7 +46,6 @@ public class MenagerBorderLayout {
 
         panelTestowy.add(button1, BorderLayout.PAGE_START);
         panelTestowy.add(button2, BorderLayout.PAGE_END);
-        //panelTestowy.add(button3, BorderLayout.LINE_START);
         panelTestowy.add(yellow, BorderLayout.LINE_START);
         panelTestowy.add(button4, BorderLayout.LINE_END);
         panelTestowy.add(button5, BorderLayout.CENTER);
@@ -51,7 +54,7 @@ public class MenagerBorderLayout {
     public MenagerBorderLayout () {
         JFrame myWindow = new JFrame("Manager BorderLayout");
         myWindow.setContentPane(createContentPane());
-        myWindow.setSize(300, 400);
+        myWindow.setSize(900, 400);
 
         myWindow.setLocationRelativeTo(null);
         myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
