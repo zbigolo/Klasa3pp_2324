ae5030dc5c32b80bc27856d06a5ec9265cbe4711
Zbigniew Gołębiewski <zbigolo@gazeta.pl>
2024-02-13 09:26:15 +0100

    update MenagerBorderLayout.java
    Signed-off-by: zbigolo <zbigolo@gazeta.pl>

--- src/Z5Managers/MenagerBorderLayout.java
+++ src/Z5Managers/MenagerBorderLayout.java
@@ -1,15 +1,21 @@
 package Z5Managers;
 
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MenagerBorderLayout {
-    public JPanel createContentPane(){
 
+
+    public JPanel createContentPane(){
         BorderLayout layoutborder = new BorderLayout();
         JPanel panelTestowy = new JPanel(layoutborder);
 
@@ -22,25 +28,34 @@ public class MenagerBorderLayout {
         JButton button3A = new JButton("Button 3A - RED");
         button3A.setBorder(new EmptyBorder(3, 0, 3, 0));
         JButton button3B = new JButton("Button 3B - GREEN");
-        button3B.setBorder(new LineBorder(Color.white, 3,true));
+        button3B.setBorder(new LineBorder(Color.white, 3,false));
 
         JPanel yellow = new JPanel(new FlowLayout());
         yellow.setBackground(Color.yellow);
+        yellow.setPreferredSize(new Dimension(200,200));
         yellow.add(button3);
         yellow.add(button3A);
         yellow.add(button3B);
 
-        button3A.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed (ActionEvent e) {
-                yellow.setBackground(Color.red);
-            }
+        button3A.addActionListener(e ->{
+                                                                    yellow.setBackground(Color.red);
+                                                                    button3A.setText("Buttona 3a - r e d");
+                                                                    button4.isDefaultButton();
         });
+
         button3B.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed (ActionEvent e) {
                 yellow.setBackground(Color.green);
             }
+        });
+        button4.addActionListener(new ActionListener() {
+                                      @Override
+                                      public void actionPerformed(ActionEvent e) {
+                                          button3A.setText("Buttona 3A - RED");
+                                          yellow.setBackground(new Color(100,100,100));
+                                          button4.setBackground(Color.white);
+                                      }
         });
 
 
@@ -54,8 +69,7 @@ public class MenagerBorderLayout {
     public MenagerBorderLayout () {
         JFrame myWindow = new JFrame("Manager BorderLayout");
         myWindow.setContentPane(createContentPane());
-        myWindow.setSize(900, 400);
-
+        myWindow.setSize(new Dimension(700,200));
         myWindow.setLocationRelativeTo(null);
         myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         myWindow.setVisible(true);
