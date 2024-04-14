1e133c938c48ebd1623d0c6a3c7ecb169d627b16
Zbyszek <zbigolo@gazeta.pl>
2024-04-15 01:39:22 +0200

    new file menu application

---
+++ src/pesel/start.java
@@ -0,0 +1,10 @@
+/**********
+ * @Author: Hubert Strączewski *
+ * @Data: 01.03.2024 *
+ **********/
+
+public class start {
+    public static void main(String[] args) {
+        new pesel();
+    }
+}
